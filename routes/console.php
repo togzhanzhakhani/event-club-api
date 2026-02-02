a5e7744bfc1e485957d74cb5b6eb1b50af53a81f
Chloe Edelweiss <to7zhan@gmail.com>
2026-02-02 20:02:30 +0500

    events archive

--- routes/console.php
+++ routes/console.php
@@ -7,4 +7,3 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-Schedule::command('events:archive-past')->hourlyAt(1);
